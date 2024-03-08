7a152acd048460fa4b0fefc265fa49f018b1c9fb
masonmoralez <moralesmason977@gmail.com>
2024-03-08 10:25:54 -1000

    Changed the Website
    yes

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("myForm").addEventListener("submit", function(event) {
+//         event.preventDefault(); // Prevent form submission
+
+       
+//         // Get form inputs
+//         var name = document.getElementById("name").value;
+//         var email = document.getElementById("email").value;
+//         var phone = document.getElementById("phone").value;
+//         var message = document.getElementById("message").value;
+  
+//         // Create data object
+//         var data = {
+//             name: name,
+//             email: email,
+//             phone: phone,
+//             message: message
+//         };
+// }};

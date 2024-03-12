ac8abb2e4bf228707eed754e4515d3a84c6a0659
the-Turtle-From-1871 <103733372+the-Turtle-From-1871@users.noreply.github.com>
2024-03-12 10:05:43 -1000

    did sum stuf
    Made it so that the user can record audio. Added second page for Ivey and Brix and styled it.

--- script.js
+++ script.js
@@ -17,3 +17,38 @@
 //             message: message
 //         };
 // }};
+
+let mediaRecorder;
+let recordedChunks = [];
+
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const audioElement = document.getElementById('audioElement');
+
+startButton.addEventListener('click', startRecording);
+stopButton.addEventListener('click', stopRecording);
+
+async function startRecording() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = function(event) {
+        recordedChunks.push(event.data);
+    }
+
+    mediaRecorder.onstop = function() {
+        const blob = new Blob(recordedChunks, { type: 'audio/wav' });
+        const url = URL.createObjectURL(blob);
+        audioElement.src = url;
+    }
+
+    mediaRecorder.start();
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
